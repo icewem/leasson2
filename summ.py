8152e6a82e148b9ef17021c9d8b7a3338d1a70fa
Шестопалов Артём Викторович <artem.shestopalov@tele2.ru>
2019-02-28 17:35:58 +0300

    v2

--- summ.py
+++ summ.py
@@ -2,5 +2,5 @@
 
 list_str = {"1","3","5","7","9","11","13","15","17","19"}
 for go_screen in list_str:
-    summ = 0 + int(go_screen)
-    print(summ)
+    go_screen = 0 + int(go_screen)
+    print(go_screen)
