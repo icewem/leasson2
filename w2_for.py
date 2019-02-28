63392f2798f703f0817eec9c5f1004c391e447bd
Шестопалов Артём Викторович <artem.shestopalov@tele2.ru>
2019-02-28 17:08:11 +0300

    добавил циклы

---
+++ w2_for.py
@@ -0,0 +1,41 @@
+# Создать список из десяти целых чисел.
+# Вывести на экран каждое число, увеличенное на 1.
+
+list_str = {"1","3","5","7","9","11","13","15","17","19"}
+for go_screen in list_str:
+    go_screen = int(go_screen) + 1  
+    print(go_screen)
+
+
+# Ввести с клавиатуры строку.
+# Вывести эту же строку вертикально: по одному символу на строку консоли.
+
+input_str = input("Приветствую.Введите строку, будет фокус:")
+
+for go_down in input_str:
+    print(go_down)
+
+
+# Создать список из словарей с оценками учеников разных классов школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
+# Посчитать и вывести средний балл по всей школе.
+# Посчитать и вывести средний балл по каждому классу.
+
+shool_perfomance = [
+    {'school_class': '1a', 
+    'scores': [3,4,4,5,2]},
+    {'school_class': '1b', 
+    'scores': [3,4,4,5,4,4,2,3,5,5,4,3]},
+    {'school_class': '2a', 
+    'scores': [5,3,5,4,3,3,5,4]},
+    {'school_class': '2b', 
+    'scores': [2,3,5,5,5,2]},
+    {'school_class': '3a', 
+    'scores': [5,4,4,2,3,5,4,5,2]},
+    {'school_class': '4a', 
+    'scores': [3,4,5,5,4,3]},
+    {'school_class': '5b', 
+    'scores': [5,5,4,3,5,2,4,5,2]}
+]
+for average_scores in shool_perfomance:
+    shool_perfomance = shool_perfomance['scores']
+    print(shool_perfomance)
