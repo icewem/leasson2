9ae5159954f1aab99677bb4fa7769ff34fc55f25
Шестопалов Артём Викторович <artem.shestopalov@tele2.ru>
2019-03-01 10:41:22 +0300

    its work sum?

--- summ.py
+++ summ.py
@@ -3,16 +3,16 @@
 marks = [5, 5, 4, 5, 3, 3, 2, 5, 4]
 marks_sum = 0
 for mark in marks:
-   marks_sum = marks_sum + marks  # тут должно быть что-то с mark и marks_sum
+    marks_sum = marks_sum + mark # тут должно быть что-то с mark и marks_sum
 
 print(marks_sum) # тут должно быть 36
-
 
 # 2 часть
 marks = {'2a': [5, 5, 4, 5, 3, 3, 2, 5, 4]}
 marks_sum = 0
 marks_in2a = marks['2a']
 
-for go_sum in marks_in2a
- marks_sum = marks_sum + marks_in2a
+for mark in marks_in2a:
+ marks_sum = marks_sum + mark
 print(marks_sum)  # тут должно быть 36, сумма оценок 2а
+
