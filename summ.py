a45cbaca6613a203941af2433bcb532e0e9786a8
Шестопалов Артём Викторович <artem.shestopalov@tele2.ru>
2019-03-01 10:22:39 +0300

    commit

--- summ.py
+++ summ.py
@@ -1,17 +1,18 @@
 # сложить числа
 
-#marks = [5, 5, 4, 5, 3, 3, 2, 5, 4]
-#marks_sum = 0
-#for mark in marks:
-#   marks_sum = marks_sum + marks  # тут должно быть что-то с mark и marks_sum
+marks = [5, 5, 4, 5, 3, 3, 2, 5, 4]
+marks_sum = 0
+for mark in marks:
+   marks_sum = marks_sum + marks  # тут должно быть что-то с mark и marks_sum
 
-#print(marks_sum) # тут должно быть 36
+print(marks_sum) # тут должно быть 36
 
 
 # 2 часть
 marks = {'2a': [5, 5, 4, 5, 3, 3, 2, 5, 4]}
-print(marks['2a'])
 marks_sum = 0
-for marks_sum in marks['2a']
- marks_sum = marks_sum + marks['2a']
+marks_in2a = marks['2a']
+
+for go_sum in marks_in2a
+ marks_sum = marks_sum + marks_in2a
 print(marks_sum)  # тут должно быть 36, сумма оценок 2а
