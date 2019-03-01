5e7584af63e01373c5d48e5838f4fc21645a3607
Артём Шестопалов <icewem@MacBook-Pro-Artem.local>
2019-03-01 23:07:36 +0300

    for is ready

--- w2_for.py
+++ w2_for.py
@@ -1,6 +1,6 @@
 # Создать список из десяти целых чисел.
 # Вывести на экран каждое число, увеличенное на 1.
-'''
+
 list_str = {"1","3","5","7","9","11","13","15","17","19"}
 for go_screen in list_str:
     go_screen = int(go_screen) + 1  
@@ -21,8 +21,6 @@ input_str = input("Приветствую.Введите строку, буде�
 for go_down in input_str:
     print(go_down)
 
-'''
-
 # Создать список из словарей с оценками учеников разных классов школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
 # Посчитать и вывести средний балл по всей школе.
 # Посчитать и вывести средний балл по каждому классу.
@@ -42,31 +40,18 @@ shool_perfomance = [
     'scores': [3,4,5,5,4,3]},
     {'school_class': '5b', 
     'scores': [5,5,4,3,5,2,4,5,2]}]
-
+all_score = 0
+all_student = 0
 for shool_param in shool_perfomance:
-    print(shool_param)
-
-
-
-'''
-
-scool = {'school_class': '1a', 'scores': [3, 4, 4, 5, 2]}
-result = 0
-fot test in scool['scores']:
-    result += test
-print(a)
-
-
-marks = {
-    '2a': [5, 5, 4, 5, 3, 3, 2, 5, 4], 
-    '3b': [4, 5, 2, 5, 3]
-    }
-
-
-for class_name,class_marks in marks.items():
-    
+    class_crores = shool_param['scores']
+    class_name = shool_param['school_class']
     marks_sum = 0
-    for summ_marks in class_marks:
-        marks_sum += summ_marks
-    print(class_name,marks_sum)
-'''
+    middle_class = len(class_crores)
+    for summ_class in class_crores:
+        marks_sum += summ_class
+    delta_class = marks_sum / middle_class
+    print('Класс: {} имеет средний балл:{}'.format(class_name,delta_class))
+    all_score += marks_sum
+    all_student += middle_class
+summ_shool = all_score / all_student
+print('Средний балл по школе равен {}'.format(summ_shool))
