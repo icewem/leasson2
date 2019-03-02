a56e34ccf157caa1582fe738b6da00c58ebc263e
Артём Шестопалов <icewem@MacBook-Pro-Artem.local>
2019-03-03 00:10:15 +0300

    Поправил замечани

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,11 @@
 def cut_cake(parts):
     try:
         return 1 / int(parts)
-    except (ZeroDivisionError,TypeError,ValueError):
+    except ZeroDivisionError:
         return 'зачем делить на ноль?'
-
-cake = cut_cake("dfdfdf")
+    except TypeError:
+        return 'ошибка типа'
+    except ValueError:
+        return 'ошибка значения'
+cake = cut_cake("?")
 print(cake)
